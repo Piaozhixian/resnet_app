a61bf468d80fe9f34f091082a816f9f948f584fe
Piao <pakushiken@gmail.com>
2022-02-21 20:40:11 +0900

    Initial Commit

---
+++ Resnet50/Resnet50App.swift
@@ -0,0 +1,17 @@
+//
+//  Resnet50App.swift
+//  Resnet50
+//
+//  Created by Zhixian Piao on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Resnet50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
